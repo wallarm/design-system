18c4fd343e290e1faf035a26cf0184b2ac8b8c53
Yanni Mamporiya <yamamporiya@wallarm.com>
2026-02-05 19:52:35 +0000

    test: Alert component [update-screenshots]

--- packages/design-system/playwright.config.ts
+++ packages/design-system/playwright.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from '@playwright/test';
 
 import { baseConfig } from '@wallarm/playwright-config/base';
 
+const DEFAULT_URL = 'http://localhost:6006';
+const envUrl = process.env.STORYBOOK_URL || DEFAULT_URL;
+const baseURL = envUrl.endsWith('/') ? envUrl : `${envUrl}/`;
+
 export default defineConfig({
   ...baseConfig,
   testDir: './src',
   testMatch: '**/*.e2e.ts',
   use: {
     ...baseConfig.use,
-    baseURL: 'http://localhost:6006',
+    baseURL,
   },
 });
