053c89d69e15be6b74d641bd4fce366a75bab466
Yanni Mamporiya <yamamporiya@wallarm.com>
2026-06-10 21:59:26 +0100

    fix: WDS-122 numeric badge [update-screenshots]

--- packages/design-system/src/components/NumericBadge/NumericBadge.tsx
+++ packages/design-system/src/components/NumericBadge/NumericBadge.tsx
@@ -1,37 +1,31 @@
 import type { FC, HTMLAttributes } from 'react';
-import { cva, type VariantProps } from 'class-variance-authority';
+import type { VariantProps } from 'class-variance-authority';
 import { cn } from '../../utils/cn';
 import type { TestableProps } from '../../utils/testId';
-
-const numericBadgeVariants = cva(
-  'inline-flex items-center rounded-full border-1 px-4 py-1 font-mono text-xs font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-zinc-950 focus:ring-offset-2',
-  {
-    variants: {
-      type: {
-        primary: 'border-transparent bg-states-primary-active text-text-primary',
-        'primary-alt': 'border-transparent bg-states-primary-alt-active text-text-primary-alt',
-        brand: 'border-bg-fill-brand bg-bg-fill-brand text-text-primary-alt',
-        destructive: 'border-bg-fill-danger bg-bg-fill-danger text-text-primary-alt',
-        outline: 'border-border-primary bg-component-outline-button-bg text-text-primary',
-        info: 'border-states-info-active bg-states-info-active text-text-info',
-      },
-    },
-  },
-);
+import { numericBadgeVariants } from './classes';
 
 type NumericBadgeNativeProps = HTMLAttributes<HTMLDivElement>;
 
-type NumericBadgeVariantsProps = VariantProps<typeof numericBadgeVariants>;
+type NumericBadgeVariantsProps = Omit<VariantProps<typeof numericBadgeVariants>, 'clickable'>;
 
 export type NumericBadgeProps = NumericBadgeNativeProps & NumericBadgeVariantsProps & TestableProps;
 
-export const NumericBadge: FC<NumericBadgeProps> = ({ type = 'primary', ...props }) => {
+export const NumericBadge: FC<NumericBadgeProps> = ({
+  type = 'primary',
+  size = 'default',
+  onClick,
+  ...props
+}) => {
+  const isClickable = !!onClick;
+
   return (
     <div
       {...props}
+      tabIndex={isClickable ? 0 : undefined}
       data-slot='numeric-badge'
       data-type={type}
-      className={cn(numericBadgeVariants({ type }))}
+      onClick={onClick}
+      className={cn(numericBadgeVariants({ type, size, clickable: isClickable }))}
     />
   );
 };
