7bd6772f23684f0837c44f513ef38600e9636981
Yanni Mamporiya <yamamporiya@wallarm.com>
2026-06-15 22:16:11 +0100

    feat: WDS-132 search

--- packages/design-system/src/components/AppShell/story-content/_storyHeaderActions.tsx
+++ packages/design-system/src/components/AppShell/story-content/_storyHeaderActions.tsx
@@ -1,20 +1,15 @@
 import { Bell, ChevronUpDown } from '../../../icons';
 import { Button } from '../../Button';
 import { Code } from '../../Code';
-import { Kbd } from '../../Kbd';
 import { Text } from '../../Text';
 import { Tooltip, TooltipContent, TooltipTrigger } from '../../Tooltip';
 import { TopHeaderSeparator } from '../../TopHeader';
 import { QuickHelpDropdown } from './_storyQuickHelpDropdown';
+import { SearchModal } from './_storySearchModal';
 
 export const HeaderActions = () => (
   <>
-    <Button variant='ghost' size='small' color='neutral' className='p-4 gap-6 rounded-6'>
-      <Code size='s' color='secondary'>
-        Search Wallarm
-      </Code>
-      <Kbd size='xsmall'>⌘ K</Kbd>
-    </Button>
+    <SearchModal />
 
     <TopHeaderSeparator />
 
