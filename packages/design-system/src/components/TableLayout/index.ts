bcaf2a647da741044f0032aa9058e01bfec8a2c5
Aleksandr Butusov <abutusov@wallarm.com>
2026-06-29 19:36:36 +0200

    feat(table-layout): public exports and Tier-0 integration test

---
+++ packages/design-system/src/components/TableLayout/index.ts
@@ -0,0 +1,15 @@
+export { TableLayout, type TableLayoutProps } from './primitives/TableLayout';
+export { TableLayoutBody } from './primitives/TableLayoutBody';
+export { TableLayoutCell, type TableLayoutCellProps } from './primitives/TableLayoutCell';
+export { TableLayoutColumn, type TableLayoutColumnProps } from './primitives/TableLayoutColumn';
+export { TableLayoutColumnGroup } from './primitives/TableLayoutColumnGroup';
+export { TableLayoutHead } from './primitives/TableLayoutHead';
+export { TableLayoutHeaderCell } from './primitives/TableLayoutHeaderCell';
+export { TableLayoutRow, type TableLayoutRowProps } from './primitives/TableLayoutRow';
+export type {
+  TableLayoutColumnAlign,
+  TableLayoutColumnPin,
+  TableLayoutColumnPresentation,
+  TableLayoutHandle,
+  TableLayoutScrollToRowOptions,
+} from './types';
