7d10f57641e608f71db1041d3b02ac3e999c271f
Aleksandr Butusov <abutusov@wallarm.com>
2026-06-29 19:30:59 +0200

    feat(table-layout): TableLayoutHeaderCell primitive

---
+++ packages/design-system/src/components/TableLayout/primitives/TableLayoutHeaderCell.tsx
@@ -0,0 +1,11 @@
+import { type ComponentPropsWithRef, forwardRef } from 'react';
+import { cn } from '../../../utils/cn';
+import { tableLayoutHeaderCell } from '../classes';
+
+export const TableLayoutHeaderCell = forwardRef<
+  HTMLTableCellElement,
+  ComponentPropsWithRef<'th'>
+>(({ className, ...props }, ref) => (
+  <th ref={ref} scope='col' className={cn(tableLayoutHeaderCell, className)} {...props} />
+));
+TableLayoutHeaderCell.displayName = 'TableLayoutHeaderCell';
