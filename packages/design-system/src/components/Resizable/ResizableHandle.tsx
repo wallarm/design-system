c8d74904eb6aa0535c7c4f97cd9daea277e1b3b2
Yanni Mamporiya <yamamporiya@wallarm.com>
2026-09-07 23:21:48 +0100

    feat: resizable

--- packages/design-system/src/components/Resizable/ResizableHandle.tsx
+++ packages/design-system/src/components/Resizable/ResizableHandle.tsx
@@ -1,9 +1,8 @@
 import type { FC, HTMLAttributes, Ref } from 'react';
 import { Separator, type SeparatorProps } from 'react-resizable-panels';
-import { GripVertical } from '../../icons';
 import { cn } from '../../utils/cn';
 import { type TestableProps, useTestId } from '../../utils/testId';
-import { resizableHandleGripVariants, resizableHandleVariants } from './classes';
+import { resizableHandleVariants } from './classes';
 import { useResizableContext } from './ResizableContext';
 
 export interface ResizableHandleProps
@@ -11,14 +10,11 @@ export interface ResizableHandleProps
     Pick<SeparatorProps, 'disabled' | 'disableDoubleClick'>,
     TestableProps {
   ref?: Ref<HTMLDivElement>;
-  /** Show a visible grip icon on the handle. */
-  withHandle?: boolean;
 }
 
 export const ResizableHandle: FC<ResizableHandleProps> = ({
   ref,
   className,
-  withHandle = false,
   'data-testid': testIdProp,
   ...props
 }) => {
@@ -32,13 +28,7 @@ export const ResizableHandle: FC<ResizableHandleProps> = ({
       data-slot='resizable-handle'
       data-testid={testId}
       className={cn(resizableHandleVariants({ orientation }), className)}
-    >
-      {withHandle && (
-        <div className={resizableHandleGripVariants()} data-slot='resizable-handle-grip'>
-          <GripVertical className='size-10' />
-        </div>
-      )}
-    </Separator>
+    />
   );
 };
 
